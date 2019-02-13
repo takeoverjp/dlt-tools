2ba74a37b7975b72cd4b25ceace4a59e8e95f4d8
Kondo Takeo <takeoverjp@gmail.com>
2019-02-14 00:19:13 +0900

    Impl dlt2txt.

--- dlt2txt.c
+++ dlt2txt.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <assert.h>
 
 #include <dlt/dlt.h>
+
+#define BUFSIZE (256 * 1024)
 
 static u_char is_verbose = 0;
 
@@ -10,8 +14,32 @@ static void
 dlt2txt (const char * const path)
 {
   DltFile file;
+  char buf[BUFSIZE];
+  DltReturnValue ret;
 
-  dlt_file_init (&file, is_verbose);
+  ret = dlt_file_init (&file, is_verbose);
+  assert (ret >= DLT_RETURN_OK);
+
+  ret = dlt_file_open (&file, path, is_verbose);
+  assert (ret >= DLT_RETURN_OK);
+
+  do
+    {
+      ret = dlt_file_read (&file, is_verbose);
+    }
+  while (ret >= DLT_RETURN_OK);
+
+  for (int i = 0; i < file.counter; i++)
+    {
+      dlt_file_message (&file, i, is_verbose);
+      printf ("%d ", i);
+      dlt_message_header (&file.msg, buf, sizeof(buf), is_verbose);
+      printf ("%s ", buf);
+      dlt_message_payload (&file.msg, buf, sizeof(buf),
+                          DLT_OUTPUT_ASCII, is_verbose);
+      printf("[%s]\n", buf);
+    }
+
   dlt_file_free (&file, is_verbose);
 }
 
@@ -47,6 +75,7 @@ main (int argc, char *argv[])
     }
 
   const char *path = argv[optind];
+  assert (access (path, R_OK) == 0);
 
   dlt2txt (path);
 
