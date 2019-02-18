c36f9fd2d99a4a8fc31b6aeaae9816c424cdf7f1
Kondo Takeo <takeoverjp@gmail.com>
2019-02-18 12:32:50 +0900

    Add multiline message into testdata.dlt.

--- tests/create_testdata.c
+++ tests/create_testdata.c
@@ -11,6 +11,7 @@ main (void)
   DLT_REGISTER_CONTEXT(con_exa1, "CON", "First context");
   for (int i = 0; i < 10; i++)
     DLT_LOG(con_exa1, DLT_LOG_INFO, DLT_STRING("Hello world!"), DLT_INT32(i));
+  DLT_LOG(con_exa1, DLT_LOG_INFO, DLT_STRING("line1\nline2\nline3"));
   usleep(1000);
   DLT_UNREGISTER_CONTEXT(con_exa1);
   DLT_UNREGISTER_APP();
